d2decaa7121752463174deaa14815ec86d5c3226
Akash Thorat <116056217+akashthorat01@users.noreply.github.com>
2024-03-15 22:04:09 +0530

    Add files via upload

---
+++ Level1/Task2/script.js
@@ -0,0 +1,10 @@
+  var options = {
+    strings: ["Frontend Developer...", "Backend Developer...","Web Developer...","Java Developer...","Web Designer..."],
+    typeSpeed: 70,  // typing speed in milliseconds
+    backSpeed: 25,  // backspacing speed in milliseconds
+    showCursor: true,  // whether to show the blinking cursor
+    cursorChar: "|",  // cursor character
+    loop:true,      //loop all the text 
+  };
+
+  var typed = new Typed("#typed-output", options);
